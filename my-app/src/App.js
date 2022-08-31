f44ef261a628e79811ddb8c69eb0544e3d577713
securitybeautystar <andrews990101@outlook.com>
2022-08-30 17:13:12 -0700

    init commit

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactDom from 'react-dom';
+import {Canvas} from '@react-three/fiber';
+
+function App() {
+  return (
+    <div id="canvas-container">
+      <Canvas>
+        <ambientLight intensity={0.1}/>
+        <directionalLight color="red" position={[0,0,5]}/>
+        <mesh>
+          <boxGeometry/>
+          <meshStandardMaterial/>
+        </mesh>
+      </Canvas>
+    </div>
+  );
+}
+
+export default App;
