22c727f9eb29b3450fa0eee55aa695fa0d3305cf
securitybeautystar <andrews990101@outlook.com>
2022-08-30 17:50:54 -0700

    add plane, control, sphere

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import ReactDom from 'react-dom';
-import {Canvas} from '@react-three/fiber';
+import React from 'react';
+import {Canvas} from 'react-three-fiber';
+
+import Sphere from './utils/Sphere.Component';
+import Plane from './utils/Plane.Component';
+import Controls from './utils/Controls.Component';
 
 function App() {
   return (
-    <div id="canvas-container">
-      <Canvas>
-        <ambientLight intensity={0.1}/>
-        <directionalLight color="red" position={[0,0,5]}/>
-        <mesh>
-          <boxGeometry/>
-          <meshStandardMaterial/>
-        </mesh>
+    <div className='container'>
+      <Canvas camera={{position: [2,2,2]}}>
+        <Controls/>
+        <fog attach="fog" args={["#041830", 5, 10]} />
+        <Sphere/>
+        <Plane/>
+        <gridHelper/>
       </Canvas>
     </div>
   );
